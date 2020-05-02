37c0cc6ac835d283ea1fa140e3a2e7fbd87f16a4
haidar786 <hussnaintarar@gmail.com>
2020-05-02 23:07:42 +0500

    one div removed

--- lib/applixir.dart
+++ lib/applixir.dart
@@ -35,7 +35,6 @@ const String kAppLixir = '''
 <div id="applixir_vanishing_div" hidden>
    <iframe id="applixir_parent" allow="autoplay"></iframe>
 </div>
-</div>
     <script type='text/javascript' src="https://cdn.applixir.com/applixir.sdk3.0m.js"></script>
 
     <script type='application/javascript'>
