9fc8df38081f70708d8d3e98bed577ad9966966c
haidar786 <hussnaintarar@gmail.com>
2020-05-03 12:31:05 +0500

    remove optionals

--- lib/applixir.dart
+++ lib/applixir.dart
@@ -33,12 +33,12 @@ const String kAppLixir = '''
 <html>
 <head>
     <title>entry</title>
-        <script type='text/javascript' src="https://cdn.applixir.com/applixir.sdk3.0m.js"></script>
 </head>
 <body style="background-color: #efe4b0">
 <div id="applixir_vanishing_div" hidden>
    <iframe id="applixir_parent" allow="autoplay"></iframe>
 </div>
+    <script type='text/javascript' src="https://cdn.applixir.com/applixir.sdk3.0m.js"></script>
 
     <script type='application/javascript'>
         function adStatusCallback(status) {
@@ -48,8 +48,6 @@ const String kAppLixir = '''
 
         var options = {
             zoneId: 3033, // the zone ID from the "Games" page
-            devId: 4100, // optional: your developer ID if using s2s callback
-            gameId: 5071, // optional: the ID for this game from the "Games" page for s2s callback
             dMode: 1, // 0 for no MD5 checksum, 1 for MD5 checksum (recommended)
             fallback: 1, // 0 for no fallbacks, 1 will show fallback ads when ads-unavailable
             adStatusCb: adStatusCallback, // optional: function to provide helpful user messages
